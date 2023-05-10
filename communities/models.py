0a6adde6b0e0df24daef17c56c8556cf09875210
codesloth <djqmflsk@naver.com>
2023-05-10 14:18:00 +0900

    ADD set base

--- communities/models.py
+++ communities/models.py
@@ -1,3 +1,93 @@
 from django.db import models
+from django.conf import settings
+from communities.models import Course
+from django.utils import timezone
+from datetime import timedelta,datetime
+from django.core.validators import MinValueValidator, MaxValueValidator
+from ckeditor_uploader.fields import RichTextUploadingField
+
 
 # Create your models here.
+
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='reviews')
+    content = models.TextField()
+    star = models.IntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def created_time(self):
+        time = datetime.now(tz=timezone.utc) - self.created_at
+
+        if time < timedelta(minutes=1):
+            return '방금 전'
+        elif time < timedelta(hours=1):
+            return str(int(time.seconds / 60)) + '분 전'
+        elif time < timedelta(days=1):
+            return str(int(time.seconds / 3600)) + '시간 전'
+        elif time < timedelta(days=7):
+            time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
+            return str(time.days) + '일 전'
+        else:
+            return self.strftime('%Y-%m-%d')
+
+    def save(self, *args, **kwargs):                  
+        self.course.star = (self.course.star * self.course.reviews.count() + self.star) / (self.course.reviews.count() + 1)
+        self.course.save()
+        super(Review, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kargs):
+        self.course.star = (self.course.star * self.course.reviews.count() - self.star) / (self.course.reviews.count() - 1)
+        self.course.save()
+        super(Review, self).delete(*args, **kargs)
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='comments', blank=True, null=True)
+    title = models.CharField(max_length=100)
+    content = RichTextUploadingField(blank=True,null=True)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_comments')
+    views = models.IntegerField(default=0)
+
+    category_Choices = (('qna','질문 & 답변'),('worry','고민있어요'),('study','스터디'),('etc','기타'))
+    category = models.CharField(max_length=20, choices=category_Choices)
+
+    @property
+    def created_time(self):
+        time = datetime.now(tz=timezone.utc) - self.created_at
+
+        if time < timedelta(minutes=1):
+            return '방금 전'
+        elif time < timedelta(hours=1):
+            return str(int(time.seconds / 60)) + '분 전'
+        elif time < timedelta(days=1):
+            return str(int(time.seconds / 3600)) + '시간 전'
+        elif time < timedelta(days=7):
+            time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
+            return str(time.days) + '일 전'
+        else:
+            return self.strftime('%Y-%m-%d')
+
+class Recomment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='recomments')
+    content = models.TextField()
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_recomments')
+
+    @property
+    def created_time(self):
+        time = datetime.now(tz=timezone.utc) - self.created_at
+
+        if time < timedelta(minutes=1):
+            return '방금 전'
+        elif time < timedelta(hours=1):
+            return str(int(time.seconds / 60)) + '분 전'
+        elif time < timedelta(days=1):
+            return str(int(time.seconds / 3600)) + '시간 전'
+        elif time < timedelta(days=7):
+            time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
+            return str(time.days) + '일 전'
+        else:
+            return self.strftime('%Y-%m-%d')
--- courses/models.py
+++ courses/models.py
@@ -5,7 +5,6 @@ from imagekit.processors import ResizeToFill
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils import timezone
 from datetime import timedelta,datetime
-from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 
@@ -82,51 +81,3 @@ class Review(models.Model):
         self.course.save()
         super(Review, self).delete(*args, **kargs)
 
-class Comment(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='comments', blank=True, null=True)
-    title = models.CharField(max_length=100)
-    content = RichTextUploadingField(blank=True,null=True)
-    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_comments')
-    views = models.IntegerField(default=0)
-
-    category_Choices = (('qna','질문 & 답변'),('worry','고민있어요'),('study','스터디'),('etc','기타'))
-    category = models.CharField(max_length=20, choices=category_Choices)
-
-    @property
-    def created_time(self):
-        time = datetime.now(tz=timezone.utc) - self.created_at
-
-        if time < timedelta(minutes=1):
-            return '방금 전'
-        elif time < timedelta(hours=1):
-            return str(int(time.seconds / 60)) + '분 전'
-        elif time < timedelta(days=1):
-            return str(int(time.seconds / 3600)) + '시간 전'
-        elif time < timedelta(days=7):
-            time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
-            return str(time.days) + '일 전'
-        else:
-            return self.strftime('%Y-%m-%d')
-
-class Recomment(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='recomments')
-    content = models.TextField()
-    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_recomments')
-
-    @property
-    def created_time(self):
-        time = datetime.now(tz=timezone.utc) - self.created_at
-
-        if time < timedelta(minutes=1):
-            return '방금 전'
-        elif time < timedelta(hours=1):
-            return str(int(time.seconds / 60)) + '분 전'
-        elif time < timedelta(days=1):
-            return str(int(time.seconds / 3600)) + '시간 전'
-        elif time < timedelta(days=7):
-            time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
-            return str(time.days) + '일 전'
-        else:
-            return self.strftime('%Y-%m-%d')
