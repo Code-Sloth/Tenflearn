9bf49187aa3ad5720a0d9d0a78987727518daebc
jeonghwan <ajh5251@gmail.com>
2023-05-10 20:17:56 +0900

    ADD communities/views.py

--- communities/urls.py
+++ communities/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import views
 app_name = 'communities'
 urlpatterns = [
-    # path('', views.index, name='index'),
 # 댓글 crud 
     path('comments/', views.comment, name='comment_index'),
     path('comments/<int:comment_pk>/', views.comment_detail, name='comment_detail'),
@@ -10,15 +9,15 @@ urlpatterns = [
     path('comments/<int:comment_pk>/update/', views.comment_update, name='comment_update'),
     path('comments/<int:comment_pk>/delete/', views.comment_delete, name='comment_delete'),
 
-# # 대댓글 crd
-#     path('comments/<int:comment_pk>/recomments/', views.recomment, name='recomment'),
-#     path('comments/<int:comment_pk>/recomments/create/', views.recomment_create, name='recomment_create'),
-#     path('comments/<int:comment_pk>/recomments/delete/', views.recomment_delete, name='recomment_delete'),
+# 대댓글 crd
+    path('comments/<int:comment_pk>/recomments/', views.recomment, name='recomment'),
+    path('comments/<int:comment_pk>/recomments/create/', views.recomment_create, name='recomment_create'),
+    path('comments/<int:comment_pk>/recomments/<int:recomment_pk>/delete/', views.recomment_delete, name='recomment_delete'),
 
-# # 리뷰 crd 
-#     path('reviews/', views.reviews, name='reviews_index'),
-#     path('reviews/create/', views.review_create, name='review_create'),
-#     path('reviews/<int:review_pk>/delete/', views.review_delete, name='review_delete'),
+#리뷰 crd 
+    path('reviews/', views.reviews, name='inflearn_index'),
+    path('reviews/create/', views.review_create, name='review_create'),
+    path('reviews/<int:review_pk>/delete/', views.review_delete, name='review_delete'),
 
 
 
--- communities/views.py
+++ communities/views.py
@@ -5,10 +5,10 @@ from courses.models import Course
 # Create your views here.
 
 def comment(request):
-    comments = Comment.objects.all()
+    reviews = Comment.objects.all()
 
     context = {
-        'comments': comments,
+        'reviews': reviews,
     }
 
     return render(request, 'communities/comment_index.html', context)
@@ -23,19 +23,17 @@ def comment_detail(request, comment_pk):
     return render(request, 'communities/comment_detail.html', context)
 
 def comment_create(request):
-# def comment_create(request, course_pk):
-    # if course_pk:
-    #     course = Course.objects.get(pk=course_pk)
-    # else:
-    #     course =
-        
-
-
+    course_pk = request.GET.get('course_pk', '0')
+    
     if request.method == 'POST':
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
-            # comment.course = course
+            if course_pk == '0':
+                pass
+            else:
+                course = Course.objects.get(pk=course_pk)
+                comment.course = course
             comment.user = request.user
             comment.save()
 
@@ -74,3 +72,93 @@ def comment_delete(request, comment_pk):
     if request.user == comment.user and request.method == 'POST':
         comment.delete()
         return redirect('communities:comment_index')
+    
+
+## 대댓글
+
+def recomment(request, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    recomments = comment.recomment_set.all()
+
+    context = {
+        'comment': comment,
+        'recomments': recomments,
+    }
+
+    return render(request, 'communities/comment_detail.html', context)
+
+def recomment_create(request, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+
+    if request.method == 'POST':
+        recomment_form = RecommentForm(request.POST)
+        if recomment_form.is_valid():
+            recomment = recomment_form.save(commit=False)
+            recomment.comment = comment
+            recomment.user = request.user
+            recomment.save()
+
+            return redirect('communities:recomment', comment_pk=comment_pk)
+
+    else:
+        recomment_form = RecommentForm()
+
+    context = {
+        'comment': comment,
+        'recomment_form': recomment_form,
+    }
+
+    return render(request, 'communities/recomment_create.html', context)
+
+
+def recomment_delete(request, comment_pk, recomment_pk):
+    recomment = Recomment.objects.get(pk=recomment_pk)
+
+    if request.user == recomment.user and request.method == 'POST':
+        recomment.delete()
+
+    return redirect('communities:recomment', comment_pk=comment_pk)
+
+
+
+
+
+## 리뷰
+
+def review(request):
+    reviews = Review.objects.all()
+
+    context = {
+        'reviews': reviews,
+    }
+
+    return render(request, 'communities\inflearn_index.html', context)
+
+
+def review_create(request):
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.user = request.user
+            review.save()
+
+            return redirect('#')
+
+    else:
+        review_form = ReviewForm()
+    context = {
+        'review_form': review_form,
+    }
+
+    return render(request, 'communities/comment_create.html', context)
+
+
+
+def review_delete(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    if request.user == review.user and request.method == 'POST':
+        review.delete()
+        return redirect('#')
+    
+
