b3972b53304400c1006f4845afd20cdd413d99f9
sotthang <sotthang@gmail.com>
2023-05-10 15:52:44 +0900

    ADD urls, views

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'courses/course_index.html')
+
+def detail(request, course_pk):
+    course = Course.objects.get(pk=course_pk)
+    context = {
+        'course': course,
+    }
+    return render(request, 'courses/course_detail.html', context)
+
+
+def comment(request):
+    return render(request, 'courses/course_comment.html')
+
+
+def courses(request):
+    return render(request, 'courses/course_courses.html')
