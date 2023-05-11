36a3def39c022f3ecd033cb77b717d68bcaa3330
hany0829 <hany8092@naver.com>
2023-05-11 10:40:14 +0900

    ADD accounts_setting

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
-
+from .import views
 app_name = 'accounts'
 urlpatterns = [
+    path('login/', views.login, name = 'login'),
+    path('logout/', views.logout, name = 'logout'),
+    path('signup/', views.signup, name = 'signup'),
+    path('update/', views.update, name = 'update'),
+    path('password/', views.change_password, name = 'change_password'),
+    # path('mypage/<username>/', views.mypage, name = 'mypage'),
 ]
