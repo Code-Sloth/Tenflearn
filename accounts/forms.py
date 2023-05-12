82869a6cbd75267c582b7884cd0324c1229394fe
jeonghwan <ajh5251@gmail.com>
2023-05-12 17:49:13 +0900

    UPDATE signup, change password 회원가입, 비밀번호 변경 views 수정, forms 수정, templates 수정

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm, PasswordChangeForm
 
 class CustomAutentication(AuthenticationForm):
     username = forms.CharField(
@@ -27,16 +27,94 @@ class CustomAutentication(AuthenticationForm):
         fields = ('username', 'password')
 
 class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        label="아이디",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "아이디",
+                "style": "width: 312px; height: 47px;",
+            }),
+    )
+
+    last_name = forms.CharField(
+        label = "성",
+        widget = forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "홍",
+                "style": "width: 312px; height: 47px;",
+            }),
+    )
+
+    first_name = forms.CharField(
+        label = "이름",
+        widget = forms.TextInput(
+            attrs = {
+                "class": "form-control",
+                "placeholder": "길동",
+                "style": "width: 312px; height: 47px;",
+            }),
+    )
+
+    password1 = forms.CharField(
+        label="비밀번호",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "******",
+                "style": "width: 312px; height: 47px;",
+            }),
+    )
+    password2 = forms.CharField(
+        label="비밀번호 확인",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "******",
+                "style": "width: 312px; height: 47px;",
+            }),
+    )
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('username', 'last_name', 'first_name',  'password1', 'password2',)
-        widgets = {
-            'username': forms.TextInput(attrs = {'class': 'form-control custom-input-form',}),
-            'last_name': forms.TextInput(attrs={'class':'form-control custom-input-form',}),
-            'first_name': forms.TextInput(attrs={'class':'form-control custom-input-form',}),
-        }
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = get_user_model()
         fields = ('username', 'last_name', 'first_name',)
+
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(
+        label='기존 비밀번호',
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "기존 비밀번호",
+                "style": "width: 312px; height: 47px;",
+            }
+        ),
+    )
+    new_password1 = forms.CharField(
+        label="새 비밀번호",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "새 비밀번호",
+                "style": "width: 312px; height: 47px;",
+            }
+        ),
+        help_text="",
+    )
+    new_password2 = forms.CharField(
+        label="새 비밀번호 확인",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "새 비밀번호 확인",
+                "style": "width: 312px; height: 47px;",
+            }
+        ),
+        help_text="",
+    )
