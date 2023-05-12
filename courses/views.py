69ed6a3d7e302f92b45c47150b004b259bb8cbef
sotthang <sotthang@gmail.com>
2023-05-12 11:13:26 +0900

    ADD courses index, detail course logic change

--- courses/views.py
+++ courses/views.py
@@ -17,17 +17,14 @@ def index(request):
     courses = Course.objects.all().order_by('-pk')
     sorted_star_courses = Course.objects.all().order_by('-star')
     sorted_enrolment_courses = Course.objects.annotate(num_enrolment_users=Count('enrolment_users')).order_by('-num_enrolment_users')
-    # similar_courses = Course.objects.filter(
-    #     Q(tags__in=courses.tags.all()) &
-    #     ~Q(enrolment_users=request.user)
-    # )
-    
-    
+    enrolled_courses = Course.objects.filter(enrolment_users=request.user)
+    enrolled_course_tags = enrolled_courses.values_list('tags', flat=True).distinct()
+    similar_courses = Course.objects.filter(Q(tags__in=enrolled_course_tags) & ~Q(enrolment_users=request.user)).distinct()
     context = {
         'courses': courses,
         'sorted_star_courses': sorted_star_courses,
         'sorted_enrolment_courses' : sorted_enrolment_courses,
-        # 'similar_courses': similar_courses,
+        'similar_courses': similar_courses,
     }
     return render(request, 'courses/course_index.html', context)
 
@@ -36,12 +33,18 @@ def detail(request, course_pk):
     course = Course.objects.get(pk=course_pk)
     reviews = Review.objects.filter(course_id=course_pk)
     review_form = ReviewForm()
-    if Course.objects.count() > 3:
-        other_courses = random.sample(list(Course.objects.all().exclude(pk=course.pk)), 3)
+    other_courses = Course.objects.filter(user=course.user).exclude(pk=course.pk)
+    if other_courses.count() > 3:
+        other_courses = random.sample(list(other_courses), 3)
     else:
-        other_courses = []
-    similar_courses = Course.objects.filter(tags__in=course.tags.all()).exclude(pk=course.pk)
-    similar_course = random.choice(similar_courses) if similar_courses else None
+        other_courses = list(other_courses)
+    similar_courses = Course.objects.filter(
+        Q(tags__in=list(course.tags.all())) &
+        ~Q(enrolment_users=request.user) &
+        ~Q(pk=course.pk)
+    ).distinct()
+    if similar_courses.count() >= 3:
+        similar_courses = random.sample(list(similar_courses), 3)
     star_percentage = []
     if reviews:
         for x in range(1, 6):
@@ -53,7 +56,7 @@ def detail(request, course_pk):
         'reviews': reviews,
         'review_form': review_form,
         'other_courses': other_courses,
-        'similar_course': similar_course,
+        'similar_courses': similar_courses,
         'star_percentage': star_percentage,
     }
     return render(request, 'courses/course_detail.html', context)
@@ -132,5 +135,9 @@ def review_delete(request, course_pk, review_pk):
     
 
 def video(request, course_pk):
-    return render(request, 'courses/course_video.html')
+    course = Course.objects.get(pk=course_pk)
+    context = {
+        course
+    }
+    return render(request, 'courses/course_video.html', context)
 
