4dbf358d2baf3c2f962634da6ef1a54fc788a7a8
jeonghwan <ajh5251@gmail.com>
2023-05-12 23:00:44 +0900

    UPDATE login, update, signup, changepassword design 로그인, 회원정보수정, 회원가입, 비밀번호 변경 화면 css 추가 및 수정

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,7 +8,7 @@ class CustomAutentication(AuthenticationForm):
         widget= forms.TextInput(attrs = {
             'class':'form-control',
             "placeholder": "아이디",
-            "style": "width: 312px; height: 47px;",
+            "style": "height: 47px;",
             "autocomplete": "username",
             }),
     )
@@ -17,7 +17,7 @@ class CustomAutentication(AuthenticationForm):
         widget = forms.PasswordInput(attrs = {
             'class':'form-control',
             "placeholder": "비밀번호",
-            "style": "width: 312px; height: 47px;",
+            "style": "height: 47px;",
             "autocomplete": "current-password",
             }),
     )
@@ -33,7 +33,7 @@ class CustomUserCreationForm(UserCreationForm):
             attrs={
                 "class": "form-control",
                 "placeholder": "아이디",
-                "style": "width: 312px; height: 47px;",
+                "style": "height: 47px;",
             }),
     )
 
@@ -43,7 +43,7 @@ class CustomUserCreationForm(UserCreationForm):
             attrs={
                 "class": "form-control",
                 "placeholder": "홍",
-                "style": "width: 312px; height: 47px;",
+                "style": "height: 47px;",
             }),
     )
 
@@ -53,7 +53,7 @@ class CustomUserCreationForm(UserCreationForm):
             attrs = {
                 "class": "form-control",
                 "placeholder": "길동",
-                "style": "width: 312px; height: 47px;",
+                "style": "height: 47px;",
             }),
     )
 
@@ -63,7 +63,7 @@ class CustomUserCreationForm(UserCreationForm):
             attrs={
                 "class": "form-control",
                 "placeholder": "******",
-                "style": "width: 312px; height: 47px;",
+                "style": "height: 47px;",
             }),
     )
     password2 = forms.CharField(
@@ -72,7 +72,7 @@ class CustomUserCreationForm(UserCreationForm):
             attrs={
                 "class": "form-control",
                 "placeholder": "******",
-                "style": "width: 312px; height: 47px;",
+                "style": "height: 47px;",
             }),
     )
     class Meta(UserCreationForm.Meta):
@@ -80,9 +80,29 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ('username', 'last_name', 'first_name',  'password1', 'password2',)
 
 class CustomUserChangeForm(UserChangeForm):
+    last_name = forms.CharField(
+        label = "성",
+        widget = forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "홍",
+                "style": "height: 47px;",
+            }),
+    )
+
+    first_name = forms.CharField(
+        label = "이름",
+        widget = forms.TextInput(
+            attrs = {
+                "class": "form-control",
+                "placeholder": "길동",
+                "style": "height: 47px;",
+            }),
+    )
+    password = None
     class Meta(UserChangeForm.Meta):
         model = get_user_model()
-        fields = ('username', 'last_name', 'first_name',)
+        fields = ('last_name', 'first_name',)
 
 
 class CustomPasswordChangeForm(PasswordChangeForm):
@@ -92,7 +112,7 @@ class CustomPasswordChangeForm(PasswordChangeForm):
             attrs={
                 "class": "form-control",
                 "placeholder": "기존 비밀번호",
-                "style": "width: 312px; height: 47px;",
+                "style": "height: 47px;",
             }
         ),
     )
@@ -102,7 +122,7 @@ class CustomPasswordChangeForm(PasswordChangeForm):
             attrs={
                 "class": "form-control",
                 "placeholder": "새 비밀번호",
-                "style": "width: 312px; height: 47px;",
+                "style": "height: 47px;",
             }
         ),
         help_text="",
@@ -113,7 +133,7 @@ class CustomPasswordChangeForm(PasswordChangeForm):
             attrs={
                 "class": "form-control",
                 "placeholder": "새 비밀번호 확인",
-                "style": "width: 312px; height: 47px;",
+                "style": "height: 47px;",
             }
         ),
         help_text="",
