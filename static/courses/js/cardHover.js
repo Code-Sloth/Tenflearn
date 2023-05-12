44b62d1215461633cd98e21960b9fda143147be9
ming0830 <minsun0830@naver.com>
2023-05-13 02:41:54 +0900

    FEAT course templates layout design

--- static/courses/js/cardHover.js
+++ static/courses/js/cardHover.js
@@ -14,15 +14,3 @@ cards.forEach((card) => {
 
 
 })
-// postImgs.forEach((postImg) => {
-//   postImg.addEventListener("mouseenter", () => {
-//     const postBottom = postImg.nextElementSibling
-//     postBottom.classList.remove('card-bottom-hidden')
-//     });
-
-//   postImg.addEventListener("mouseleave", () => {
-//     const postBottom = postImg.nextElementSibling
-//     postBottom.classList.add('card-bottom-hidden')
-//     });
-
-// })
