a244fd46f1b6902e5fce5667dc61d54ebaebd463
sotthang <sotthang@gmail.com>
2023-05-16 15:17:04 +0900

    ADD course video url

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Course, Review
+from .models import Course, Review, Url
 
 # Register your models here.
 
 admin.site.register(Course)
 admin.site.register(Review)
+admin.site.register(Url)
