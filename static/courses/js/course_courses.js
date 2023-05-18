7560665195a2a4b1e47d60c6c08becf272bad7e7
ming0830 <minsun0830@naver.com>
2023-05-18 16:48:47 +0900

    UPDATE toggle

--- static/courses/js/course_courses.js
+++ static/courses/js/course_courses.js
@@ -1,14 +1,22 @@
+toggleBtn();
+
 export function toggleBtn() {
   const toggleBtn = document.querySelector('.skill-tags-more');
-  
+
   toggleBtn.addEventListener('click', () => {
     const skillTagsContainer = document.querySelector('.skill-tags-container');
     if (!skillTagsContainer.classList.contains('overflow-hidden')) {
       skillTagsContainer.classList.add('overflow-hidden');
-      toggleBtn.classList.replace('bi-chevron-compact-up','bi-chevron-compact-down')
+      toggleBtn.classList.replace(
+        'bi-chevron-compact-up',
+        'bi-chevron-compact-down'
+      );
     } else {
       skillTagsContainer.classList.remove('overflow-hidden');
-      toggleBtn.classList.replace('bi-chevron-compact-down','bi-chevron-compact-up')
+      toggleBtn.classList.replace(
+        'bi-chevron-compact-down',
+        'bi-chevron-compact-up'
+      );
     }
   });
 }
